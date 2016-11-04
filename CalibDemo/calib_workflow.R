c5a936d3fbc654247446fc56a0adea04b3a56040
wrudisill <william.rudisill@gmail.com>
2016-11-04 12:34:47 -0600

    annotate calib workflow

--- CalibDemo/calib_workflow.R
+++ CalibDemo/calib_workflow.R
@@ -71,6 +71,8 @@ library(plyr)
 
 source("calib_utils.R")
 
+### WILL: sets up parallel processing
+
 # Multi-core
 parallelFlag <- TRUE
 ncores <- 8
@@ -85,6 +87,8 @@ if (parallelFlag && ncores>1) {
 # MAIN CODE
 #########################################################
 
+### WILL: proj_data.Rdata == where previous calib outputs are stored?
+
 # First loop check
 if (file.exists("proj_data.Rdata")) { 
    load("proj_data.Rdata")
@@ -97,14 +101,24 @@ if (file.exists("proj_data.Rdata")) {
    writePlotDir <- paste0(runDir, "/plots")
    dir.create(writePlotDir)
 
+   ## WILL: What goes into obs file? I'm guessing 'observations'... Yes. 
+
    # Load obs so we have them for next iteration
    load(obsFile)
-   obsDT$q_cms <- NULL
+   #obsDT$q_cms <- NULL
 
+
+###### WILL: This part will be removed/changed
+######
    # Find the index of the gage
    rtLink <- ReadRouteLink(rtlinkFile)
    rtLink <- data.table(rtLink)
    linkId <- which(trimws(rtLink$gages) %in% siteId)
+##########
+#########
+   
+   ####WILL: this chunk should stay the same. 'paramBnds' must come from
+   #### the calib.utils.R script... 
 
    # Setup value lists from paramBnds
    xnames <- paramBnds$param
@@ -114,6 +128,8 @@ if (file.exists("proj_data.Rdata")) {
    names(x_min) <- xnames
    x_max <- paramBnds$max
    names(x_max) <- xnames
+
+   #WILL: This dataframe contains all of the iterations of Parameters
 
    # Initialize parameter archive DF
    message("Initialize parameter archive")
@@ -135,6 +151,7 @@ if (file.exists("proj_data.Rdata")) {
    quit("no")
 }
 
+
 if (cyclecount > 0) {
 
    file.rename("params_new.txt", paste0("CALIB_RESULTS/OUTPUT", cyclecount, "/params_new.txt"))
@@ -145,34 +162,82 @@ if (cyclecount > 0) {
 
    # Read files
    message("Reading model out files.")
+
+########
+########
+
    system.time({
-   filesList <- list.files(path = outPath,
-                          pattern = glob2rx("*.CHRTOUT_DOMAIN*"),
-                          full.names = TRUE)
+
+
+   modelFiles <- list.files(path = outputPath, 
+                            patt=obsTimePatterns, 
+                            full=TRUE)
+
+   if (length(modelFiles) == 0) stop("No matching files in specified directory.")
+   
+   #####MODIFY: right now its reading all outputs, needs to just be LDASOUT
+   allLdasOut <- plyr::llply(modelFiles, ReadLdasOut)
+
+
+   # filesList <- list.files(path = outPath,
+   #                        pattern = glob2rx("*.CHRTOUT_DOMAIN*"),
+   #                        full.names = TRUE)
+
+########
+########
+
+   #####WILL: Should spit out dates from the outfile names...
    filesListDate <- as.POSIXct(unlist(plyr::llply(strsplit(basename(filesList),"[.]"), '[',1)), format = "%Y%m%d%H%M", tz = "UTC")
+   
+   ####WIll: this allows for model spinup, outfiles aren't read from before startdate(?)
    whFiles <- which(filesListDate >= startDate)
+
+   ###WILL Subsets outfiles that are newer than start date
    filesList <- filesList[whFiles]
    if (length(filesList) == 0) stop("No matching files in specified directory.")
+
+
+#######WILL: this will need to change... ReadChFile Refers to the util function which reads 
+#######streamflow from the output Netcdf file.  
+
    chrt <- as.data.table(plyr::ldply(filesList, ReadChFile, linkId, .parallel = parallelFlag))
    })
 
+   #######WILL: This will also need to change
    # Convert to daily
    chrt.d <- Convert2Daily(chrt)
    chrt.d[, site_no := siteId]
+
+   ###WILL: not sure what assign is doing
    assign(paste0("chrt.d.", cyclecount), chrt.d)
+   
+   ###WILL: saves the chrt (the output discharge) to .Rdata file
    save(list=c(paste0("chrt.d.", cyclecount)), file=paste0("archive/", paste0("chrt.d.", cyclecount), ".Rdata"))
 
    # Merge
+   ###WILL: where does setkey come from? 
    setkey(chrt.d, "site_no", "POSIXct")
    setkey(obsDT, "site_no", "POSIXct")
+
+   ##WILL: 'merge' merges two dataframes by parameter. This step puts obs and output 
+   ## the same dataframe..
    chrt.d <- merge(chrt.d, obsDT, by=c("site_no", "POSIXct"), all.x=FALSE, all.y=FALSE)
+   
+   ##WILL: objFn (note: not a verb) returns the objective function
    F_new <- objFn(chrt.d$q_cms, chrt.d$obs)
    print(F_new)
+
+
+
+
+#############################################################################
+#######         WILL: Below should not need to change       #################
 
    # Archive results
    x_archive[cyclecount,] <- c(cyclecount, x_new, F_new)
 
    # Evaluate performance metric
+   # WILL: recall, x_new is the vector of params
    if (cyclecount == 1) {
       x_best <- x_new
       F_best <- F_new
@@ -181,8 +246,13 @@ if (cyclecount > 0) {
       F_best <- F_new
    }
 
+   ##WILL: where does m come from?
    if (cyclecount < m) {
       # Select next parameter set
+      
+      ###WILL: DDS is the algorithm that takes the best, min, and max and finds params for 
+      ###the next timestep. where does r come from?
+
       x_new <- DDS.sel(i=cyclecount, m=m, r=r, xnames=xnames, x_min=x_min, x_max=x_max, x_best=x_best)
       cyclecount <- cyclecount+1  
 
@@ -215,6 +285,8 @@ if (cyclecount > 0) {
    }
 
    # Save and exit
+   ###WILL: what gets saved in 'save.image'?....
+
    save.image("proj_data.Rdata")
    quit("no")
 
